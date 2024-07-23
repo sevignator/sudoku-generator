f09496201e34e997bef635b48bd11b454e7cf81a
Mikaël Sévigny <sevignator@hotmail.com>
2024-07-23 11:14:20 -0400

    Remove commented out code

--- server.py
+++ server.py
@@ -8,5 +8,4 @@ sudoku_grid = retry_until_successful(lambda: SudokuGrid(3, 3))
 
 @app.route("/")
 def render_grid():
-    # return f"<p>{str(sudoku_grid.get_grid())}</p>"
     return render_template("base.html", grid=sudoku_grid.get_grid())
